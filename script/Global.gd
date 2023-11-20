5986a80da1c9849698996f8543f92290e34cff32
pedro3pv <pedro_atm3p@hotmail.com>
2023-11-20 03:05:24 -0300

    incompleto
    versão incompleta, tá fazendo quase tudo, só que meio instável, hora da uma reposta certa hora da errada, mas descobrir que o projeto está seguindo para o caminho errado por isso irei providenciar uma correção para o caminho certo, pois eu não estava seguindo o caminho certo, mas colocarei a build dessa versão chamada de versão supreme

--- script/Global.gd
+++ script/Global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var time
+var time_real
 var log : Array = []
 var carros : Array = []
 var M : int = 2
@@ -14,23 +15,29 @@ var matrix : Array = [
 func ocupar_lugar():
 	var M_temp = randi() % M
 	var N_temp = randi() % N
-	matrix[M_temp][N_temp] = true
-	return [M_temp,N_temp]
+	if !(matrix[M_temp][N_temp]):
+		matrix[M_temp][N_temp] = true
+		return [M_temp,N_temp]
+	else:
+		ocupar_lugar()
 	
 func desocupar_lugar(M,N):
 	matrix[M][N] = false
 
 func _ready():
 	time = Time.get_datetime_dict_from_system()
+	time_real = Time.get_datetime_dict_from_system()
 	print(time)
 
 func _process(delta):
 	var current_time = Time.get_datetime_dict_from_system()
-	if (current_time["minute"] - time["minute"] > 0):
-		time["minute"] += current_time["minute"] - time["minute"]
+	if (current_time["minute"] > time_real["minute"]):
+		var difereca = current_time["minute"] - time_real["minute"]
+		time_real["minute"] = current_time["minute"]
+		time["minute"] += difereca
 	if (time["minute"] >= 60):
-		time["minute"] = 0
+		time["minute"] = time["minute"] - 60
 		time["hour"] += 1
-	if (time["hour"] >= 24):
+	if (time["hour"] > 24):
 		time["hour"] = time["hour"] - 24
 		time["day"] += 1
--- script/placa.gd
+++ script/placa.gd
@@ -12,8 +12,7 @@ extends Node2D
 #MXD9B75
 #tocatins tbm, to entendendo é nada, já olhei codigo umas 5 vezes mais não achei nada
 
-var log = []
-var carros = [[]]
+var log = Global.log
 
 func _process(delta):
 	$ColorRect/time.text = str(Global.time["day"])+"/"+str(Global.time["month"])+"/"+(str(Global.time["year"])[2]+str(Global.time["year"])[3]) + " " +str(Global.time["hour"]) + ":" + str(Global.time["minute"])
@@ -23,7 +22,7 @@ func atualizar_log():
 	for placa in log:
 		text_log += placa + "\n"
 	$ColorRect/RichTextLabel.text = text_log
-	log = Global.log
+	Global.log = log
 
 func letra_para_numero(letra: String) -> int:
 	# Converte a letra para minúscula para lidar com maiúsculas e minúsculas
@@ -46,77 +45,84 @@ func letra_para_numero(letra: String) -> int:
 func verificarPlaca(placa : String):
 	var date = str(Global.time["day"])+"/"+str(Global.time["month"])+"/"+(str(Global.time["year"])[2]+str(Global.time["year"])[3]) + " " +str(Global.time["hour"]) + ":" + str(Global.time["minute"])
 	if (typeof(placa) == TYPE_STRING and !placa == ""):
-		if !(letra_para_numero(placa[0]) == -1 or letra_para_numero(placa[1]) == -1 or letra_para_numero(placa[2]) == -1):
+		var contains_placa = false
+		if !(Global.carros == []):
+			for i in Global.carros:
+				if (i[0] == placa):
+					contains_placa = true
+		if !(contains_placa) and !(letra_para_numero(placa[0]) == -1 or letra_para_numero(placa[1]) == -1 or letra_para_numero(placa[2]) == -1):
 			#mato grosso do sul - 6
 			#1
 			if (letra_para_numero(placa[0]) >= 7 and letra_para_numero(placa[1]) >= 16 and letra_para_numero(placa[2]) >= 5 and letra_para_numero(placa[0]) <= 7 and letra_para_numero(placa[1]) <= 19 and letra_para_numero(placa[2]) <= 22):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
 			#2
 			elif (letra_para_numero(placa[0]) >= 13 and letra_para_numero(placa[1]) >= 17 and letra_para_numero(placa[2]) <= 5 and letra_para_numero(placa[0]) <= 13 and letra_para_numero(placa[1]) <= 18 and letra_para_numero(placa[2]) >= 3):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
 			#3
 			elif (letra_para_numero(placa[0]) >= 14 and letra_para_numero(placa[1]) >= 14 and letra_para_numero(placa[2]) >= 6 and letra_para_numero(placa[0]) <= 14 and letra_para_numero(placa[1]) <= 14 and letra_para_numero(placa[2]) <= 20):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
 			#4
 			elif (letra_para_numero(placa[0]) >= 16 and letra_para_numero(placa[1]) >= 0 and letra_para_numero(placa[2]) >= 0 and letra_para_numero(placa[0]) <= 16 and letra_para_numero(placa[1]) <= 0 and letra_para_numero(placa[2]) <= 25):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
 			#5
 			elif (letra_para_numero(placa[0]) >= 17 and letra_para_numero(placa[1]) >= 4 and letra_para_numero(placa[2]) >= 22 and letra_para_numero(placa[0]) <= 17 and letra_para_numero(placa[1]) <= 4 and letra_para_numero(placa[2]) <= 25):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
 			#6
 			elif (letra_para_numero(placa[0]) >= 17 and letra_para_numero(placa[1]) >= 22 and letra_para_numero(placa[2]) >= 0 and letra_para_numero(placa[0]) <= 17 and letra_para_numero(placa[1]) <= 22 and letra_para_numero(placa[2]) <= 9):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso do Sul")
 			
 			#mato grosso - 8
 			#1
 			elif (letra_para_numero(placa[0]) >= 9 and letra_para_numero(placa[1]) <= 23 and letra_para_numero(placa[2]) <= 25 and letra_para_numero(placa[0]) <= 10 and letra_para_numero(placa[1]) >= 0 and letra_para_numero(placa[2]) >= 20):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			#2
 			elif (letra_para_numero(placa[0]) >= 13 and letra_para_numero(placa[1]) >= 8 and letra_para_numero(placa[2]) <= 24 and letra_para_numero(placa[0]) <= 13 and letra_para_numero(placa[1]) <= 9 and letra_para_numero(placa[2]) >= 22):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			#3
 			elif (letra_para_numero(placa[0]) >= 13 and letra_para_numero(placa[1]) >= 15 and letra_para_numero(placa[2]) >= 2 and letra_para_numero(placa[0]) <= 13 and letra_para_numero(placa[1]) <= 15 and letra_para_numero(placa[2]) <= 16):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			#4
 			elif (letra_para_numero(placa[0]) >= 13 and letra_para_numero(placa[1]) >= 20 and letra_para_numero(placa[2]) <= 23 and letra_para_numero(placa[0]) <= 13 and letra_para_numero(placa[1]) <= 20 and letra_para_numero(placa[2]) >= 6):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			#5
 			elif (letra_para_numero(placa[0]) >= 14 and letra_para_numero(placa[1]) >= 0 and letra_para_numero(placa[2]) >= 15 and letra_para_numero(placa[0]) <= 14 and letra_para_numero(placa[1]) <= 1 and letra_para_numero(placa[2]) <= 18):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			#6
 			elif (letra_para_numero(placa[0]) >= 16 and letra_para_numero(placa[1]) >= 1 and letra_para_numero(placa[2]) >= 0 and letra_para_numero(placa[0]) <= 16 and letra_para_numero(placa[1]) <= 2 and letra_para_numero(placa[2]) <= 25):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			#7
 			elif (letra_para_numero(placa[0]) >= 17 and letra_para_numero(placa[1]) >= 0 and letra_para_numero(placa[2]) >= 10 and letra_para_numero(placa[0]) <= 17 and letra_para_numero(placa[1]) <= 0 and letra_para_numero(placa[2]) <= 25):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			#8
 			elif (letra_para_numero(placa[0]) >= 17 and letra_para_numero(placa[1]) >= 17 and letra_para_numero(placa[2]) >= 8 and letra_para_numero(placa[0]) <= 17 and letra_para_numero(placa[1]) <= 17 and letra_para_numero(placa[2]) <= 25):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Mato Grosso")
 			# tocantins - 6
 			#1
 			elif (letra_para_numero(placa[0]) >= 12 and letra_para_numero(placa[1]) >= 21 and letra_para_numero(placa[2]) >= 6 and letra_para_numero(placa[0]) <= 12 and letra_para_numero(placa[1]) <= 23 and letra_para_numero(placa[2]) <= 11):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
 			#2
 			elif (letra_para_numero(placa[0]) >= 14 and letra_para_numero(placa[1]) >= 11 and letra_para_numero(placa[2]) >= 7 and letra_para_numero(placa[0]) <= 14 and letra_para_numero(placa[1]) <= 11 and letra_para_numero(placa[2]) <= 13):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
 			#3
 			elif (letra_para_numero(placa[0]) >= 14 and letra_para_numero(placa[1]) >= 24 and letra_para_numero(placa[2]) >= 0 and letra_para_numero(placa[0]) <= 14 and letra_para_numero(placa[1]) <= 24 and letra_para_numero(placa[2]) <= 2):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
 			#4
 			elif (letra_para_numero(placa[0]) >= 16 and letra_para_numero(placa[1]) >= 10 and letra_para_numero(placa[2]) >= 0 and letra_para_numero(placa[0]) <= 16 and letra_para_numero(placa[1]) <= 10 and letra_para_numero(placa[2]) <= 12):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
 			#5
 			elif (letra_para_numero(placa[0]) >= 16 and letra_para_numero(placa[1]) >= 22 and letra_para_numero(placa[2]) >= 0 and letra_para_numero(placa[0]) <= 16 and letra_para_numero(placa[1]) <= 22 and letra_para_numero(placa[2]) <= 5):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
 			#6
 			elif (letra_para_numero(placa[0]) >= 17 and letra_para_numero(placa[1]) >= 18 and letra_para_numero(placa[2]) >= 0 and letra_para_numero(placa[0]) <= 17 and letra_para_numero(placa[1]) <= 18 and letra_para_numero(placa[2]) <= 5):
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Tocantins")
 			else:
-				log.append("ID: "+ str(log.size()) +" PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Desconhecido")
-			carros.append([placa,date,Global.ocupar_lugar()])
+				log.append("PLACA: " + placa + " DATA DE ENTRADA: "+ date + " ESTADO: Desconhecido")
+			Global.carros.append([placa,Global.time["day"],Global.time["month"],Global.time["year"],Global.time["hour"],Global.time["minute"],Global.ocupar_lugar()])
 			atualizar_log()
-		else:
+		elif !(contains_placa):
 			push_error("Caracteres Invalidos")
+		else:
+			push_warning("placa repetida")
 	else:
 		push_warning("coloque uma placa")
 
@@ -141,3 +147,34 @@ func _on_hora_pressed():
 
 func _on_hora_2_pressed():
 	Global.time["hour"] += 3
+
+
+func _on_remover_pressed():
+	var date = str(Global.time["day"])+"/"+str(Global.time["month"])+"/"+(str(Global.time["year"])[2]+str(Global.time["year"])[3]) + " " +str(Global.time["hour"]) + ":" + str(Global.time["minute"])
+	if (typeof($ColorRect/TextEdit.text[0]) == TYPE_STRING):
+		var contains_placa = 0
+		if !(Global.carros == []):
+			for i in range(Global.carros.size()):
+				if (Global.carros[i][0] == $ColorRect/TextEdit.text):
+					contains_placa = i
+		var preco = 0.00
+		var permaneceu = [Global.time["hour"] - Global.carros[contains_placa][4],Global.time["minute"] - Global.carros[contains_placa][5]]
+		if ((Global.time["minute"] - Global.carros[contains_placa][5] > 15) or (Global.time["hour"] - Global.carros[contains_placa][4] > 0)):
+			Global.carros[contains_placa][4] = Global.time["hour"] - Global.carros[contains_placa][4]
+			if (Global.time["minute"] - Global.carros[contains_placa][5] > 15):
+				Global.carros[contains_placa][5] = Global.time["minute"] - Global.carros[contains_placa][5]
+			preco += 10.00
+			Global.carros[contains_placa][5] -= 15
+			Global.carros[contains_placa][4] -= 3
+			if (Global.carros[contains_placa][4] > 0):
+				preco += (2.50 * Global.carros[contains_placa][4])
+				if (Global.carros[contains_placa][5] > 0):
+					preco += 2.50
+		log.append("PLACA: "+Global.carros[contains_placa][0]+" SAIDA: "+date+" PAGOU: "+"R$ "+str(preco)+" permaneceu: "+str(permaneceu[0])+":"+str(permaneceu[1]))
+		Global.desocupar_lugar(Global.carros[contains_placa][6][0],Global.carros[contains_placa][6][1])
+		Global.carros.remove_at(contains_placa)
+		atualizar_log()
+
+
+func _on_minute_pressed():
+	Global.time["minute"] += 5
